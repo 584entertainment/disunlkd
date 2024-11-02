382854064db7a469edc9691cde09caf424a79ca6
584entertainment <584entertainment@gmail.com>
2024-11-02 11:33:25 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 function openPopup() {
-    document.getElementById("email-popup").style.display = "flex";
+    document.getElementById("popup").style.display = "flex";
 }
 
 function closePopup() {
-    document.getElementById("email-popup").style.display = "none";
+    document.getElementById("popup").style.display = "none";
 }
+
+document.getElementById("popup-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    closePopup();
+    alert("Thank you! Your free chapter will be sent to your email shortly.");
+});
