6d9c039f7822403268e375c34b20dd5a156c81c4
584entertainment <584entertainment@gmail.com>
2024-11-01 22:40:20 -0700

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function openPopup() {
+    document.getElementById("email-popup").style.display = "flex";
+}
+
+function closePopup() {
+    document.getElementById("email-popup").style.display = "none";
+}
